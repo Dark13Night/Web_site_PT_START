9fb68f04406ba9f145d975f17b5dd965c02e9369
Dark13Night <kitty7.dec.2004@gmail.com>
2024-04-25 16:02:51 +0300

    message2

---
+++ js/button1.js
@@ -0,0 +1,11 @@
+﻿var count = 0;
+document.getElementById("myButton").onclick = function () {
+    count++;
+    if (count % 2 == 0) {
+        document.getElementById("demo").innerHTML = "";
+    } else {
+        var img = document.createElement("img");
+        img.src = 'C:/PT_START/Web_site_PT_START/image/photo3.jpg';
+        document.getElementById("demo").appendChild(img);
+    }
+}
